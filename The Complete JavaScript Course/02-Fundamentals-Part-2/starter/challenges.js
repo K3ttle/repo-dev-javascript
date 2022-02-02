c13826be9423b5b02fbb6e3303586077b4712f31
Ryan Watson <r.watsontv@gmail.com>
2022-02-02 19:04:51 +1100

    Critical commit

---
+++ The Complete JavaScript Course/02-Fundamentals-Part-2/starter/challenges.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// const dolphinsScores = [44,23,71];
+// const koalasScores = [85,54,41];
+
+// function avgScore1(scores) {
+
+//     const sum = scores.reduce((t, c) => t + c);
+
+//     return Math.round(sum / scores.length);
+// }
+
+// // OR
+
+// const avgScore2 = (scores) => scores.reduce((t, c, i) => {
+//     t += c;
+//     if (i === scores.length-1) {
+//         return Math.round(t/scores.length);
+//     } else {
+//         return t;
+//     }
+// });
+
+
+// console.log(avgScore2(dolphinsScores));
---
+++ The Complete JavaScript Course/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let testBool = false;
+const testVar = true;
+
+if (testVar) {testBool = true};
+
+console.log(testBool);
