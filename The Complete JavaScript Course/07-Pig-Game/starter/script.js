efdac62b1d48bcefe682ef9222360c8ba0351e3f
Ryan Watson <r.watsontv@gmail.com>
2022-03-30 20:12:06 +1100

    pig game 1

--- The Complete JavaScript Course/07-Pig-Game/starter/script.js
+++ The Complete JavaScript Course/07-Pig-Game/starter/script.js
@@ -1,1 +1,34 @@
-'use strict';
+"use strict";
+
+////////// VARIABLES
+const dice = document.querySelector(".dice");
+const rollBtn = document.querySelector(".btn--roll");
+const holdBtn = document.querySelector(".btn--hold");
+const newGameBtn = document.querySelector(".btn--new");
+
+////////// FUNCTIONS
+function generateRandomNumber(n) {
+  return Math.ceil(Math.random() * n);
+}
+
+function ShowDice() {
+  if (dice.classList.contains("hidden")) {
+    dice.classList.remove("hidden");
+  }
+}
+
+function GenerateDice() {
+  const i = generateRandomNumber(6);
+  let imgArray = [];
+  for (let i = 1; i < 7; i++) {
+    imgArray.push(`dice-${i}.png`);
+  }
+  return imgArray[i];
+}
+
+function RollDice() {}
+
+////////// EVENTS
+rollBtn.addEventListener("click", () => {
+  ShowDice();
+});
